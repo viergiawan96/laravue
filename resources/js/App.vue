53ae0b27d3e8bfd06b91ac72f9ceed75f355e3c5
angga <47542917+viergiawan96@users.noreply.github.com>
2019-12-02 14:33:03 +0700

    commit 2

---
+++ resources/js/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+      <nav class="nav">
+          <div class="container">
+              <div class="logo">
+                  <img alt="Vue logo" src="@/assets/img/logo.png">
+              </div>
+              <div id="mainListDiv" class="main_list">
+                  <ul class="navlinks">
+                      <li><a><router-link to="/">Home</router-link></a></li>
+                      <li><a><router-link to="/about">About</router-link></a></li>
+                      <li><a href="#">Login</a></li>
+                      <li><a href="#">Register</a></li>
+                  </ul>
+              </div>
+              <span class="navTrigger">
+                  <i></i>
+                  <i></i>
+                  <i></i>
+              </span>
+          </div>
+      </nav>
+      <router-view/>
+  </div>
+</template>
+
+<style scoped>
+.main_list a.router-link-exact-active {
+  color: #00E676;
+}
+.nav {
+  z-index: 999;
+}
+</style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
     <head>
         <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-       
+        <title>Laravel Vue Router/title</title>
+        <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
-         <div id="app">
-         <example-component></example-component>
-         </div>
-         <script src="{{ asset('js/app.js') }}"></script>
+        <h1>Laravel Vue Router</h1>
+        <div id="app">
+            <router-view></router-view>
+        </div>
+        <script src="{{asset('js/app.js')}}" ></script>
     </body>
 </html>
